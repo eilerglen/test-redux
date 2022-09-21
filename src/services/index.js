c310dd75f1e8d72685907922f5d85cfb1d9779ab
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-09-21 18:43:54 +0300

    add logic of redux

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,18 @@ import { combineReducers } from "redux";
 import {todoReducer, visibleReducer} from '../services/reducers'
 import { customersReducer } from "./customersReducer";
 
-export const todoApp = combineReducers({
-  todoReducer,
-  visibleReducer,
-  customersReducer,
-})
+
+  // function todoApp (state = {}, action) {
+  //     return {
+  //       visibleReducer: visibleReducer (state.visibleStatus, action),
+  //       todoReducer: todoReducer(state.todos, action)
+  //     }
+  // }
+
+  export const todoApp = combineReducers({
+    todoReducer,
+    visibleReducer,
+    customersReducer,
+  })
 
 export default todoApp
--- src/services/reducers.js
+++ src/services/reducers.js
@@ -10,38 +10,38 @@ const { SHOW_ALL } = VisibilityFilter;
 
 
 
-export function todoReducer (state=[], action) {
-  switch (action.type) {
-    case ADD_TODO: 
-      return [
-        ...state,
-        {
-          text: action.text,
-          completed: false
+  export function todoReducer (state=[], action) {
+    switch (action.type) {
+      case ADD_TODO: 
+        return [
+          ...state,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      case TOGGLE_TODO: 
+      return state.map((todo, index) => {
+        if(index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed,
+          })
+          
         }
-      ]
-    case TOGGLE_TODO: 
-    return state.map((todo, index) => {
-      if(index === action.index) {
-        return Object.assign({}, todo, {
-          completed: !todo.completed,
-        })
-        
-      }
-      return todo;
-    })
-    default:
-      return state
-  }
-}
-
-export function visibleReducer (state = SHOW_ALL, action) {
-  switch(action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter
+        return todo;
+      })
       default:
-        return state;  
+        return state
+    }
   }
-}
+
+  export function visibleReducer (state = SHOW_ALL, action) {
+    switch(action.type) {
+      case SET_VISIBILITY_FILTER:
+        return action.filter
+        default:
+          return state;  
+    }
+  }
 
 
